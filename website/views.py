5724f5c5a28a352060341b80ce7df5d3afe85915
Nazanin-Hosseini-afzal <hosseiniafzalnazaninzahra@gmail.com>
2025-07-11 22:52:34 -0700

    created first app
    created first app named website with 3 views for contact , about and index

---
+++ website/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse 
+
+def index_view (request):
+    return HttpResponse('Home page')
+
+
+def about_view (request):
+    return HttpResponse('about page')
+
+
+def contact_view (request):
+    return HttpResponse('contact page')
